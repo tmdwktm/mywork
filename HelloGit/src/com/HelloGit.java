231d350df6af3032056c19d37f2db761db6310d2
user38 <user38@gntp-38.gntp.local>
2018-04-27 14:45:27 +0900

    입력부분 추가

--- HelloGit/src/com/HelloGit.java
+++ HelloGit/src/com/HelloGit.java
@@ -1,8 +1,14 @@
 package com;
 
+import java.util.Scanner;
+
 public class HelloGit {
 	public static void main(String args[]) {
 		System.out.println("Hello");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		String name = scanner.next();
 
 }
 }
